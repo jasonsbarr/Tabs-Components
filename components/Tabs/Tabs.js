92bfbcf1647f3d63c67572e36c74c6b07c056dce
Jason Barr <jason@jasonsbarr.com>
2019-04-18 21:08:42 -0500

    TabLink.select()

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -30,18 +30,24 @@ class TabLink {
 
   };
 
-  select() {
+  /**
+   * Take click and display TabItem content associated with the current TabLink
+   * 
+   * @param {Event} evt click event object
+   */
+  select(evt) {
     // Get all of the elements with the tabs-link class
-    // const links;
-
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach();
+    // I'm using .map() BECAUSE I CAN
+    const links = [...document.querySelectorAll('.tabs-link')]
+      .map(link => link.classList.remove('tabs-link-selected'));
+
 
     // Add a class named "tabs-link-selected" to this link
-    // this.element;
+    this.element.classList.add('tabs-link-selected');
     
     // Call the select method on the item associated with this link
-
+    this.tabItem.select();
   }
 }
 
